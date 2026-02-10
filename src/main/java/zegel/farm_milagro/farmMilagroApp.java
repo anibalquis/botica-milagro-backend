2af803f8b4f35917bb7b44d1c07df4ee62e3085b
anibalquis <159956340+anibalquis@users.noreply.github.com>
2026-02-10 09:00:47 -0500

    prepare Java backend for deployment

--- src/main/java/zegel/farm_milagro/farmMilagroApp.java
+++ src/main/java/zegel/farm_milagro/farmMilagroApp.java
@@ -2,10 +2,28 @@ package zegel.farm_milagro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class farmMilagroApp {
 	public static void main(String[] args) {
 		SpringApplication.run(farmMilagroApp.class, args);
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 }
